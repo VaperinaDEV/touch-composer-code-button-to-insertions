fe0a915b6d27376df48199f9d3d92eb1995324cf
Don <71207900+VaperinaDEV@users.noreply.github.com>
2024-02-09 13:02:47 +0100

    Create composer-code-button-insertion.js

---
+++ javascripts/discourse/initializers/composer-code-button-insertion.js
@@ -0,0 +1,23 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "composer-code-button-to-insertions",
+  
+  initialize(owner) {
+    withPluginApi("0.8.7", (api) => {
+      const capabilities = owner.lookup("capabilities:main");
+
+      if (capabilities.touch) {
+        api.onToolbarCreate(function(toolbar) {
+          toolbar.addButton({
+            id: "code",
+            group: "insertions",
+            preventFocus: true,
+            trimLeading: true,
+            action: "formatCode",
+          });
+        });
+      }
+    });
+  },
+};
